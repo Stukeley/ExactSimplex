ded70b7b32512240ea8bfa7e619e9a6cad30e676
Rafał Klinowski <klinowskirafal@gmail.com>
2023-03-14 18:39:29 +0100

    Change example problem.

--- ExactSimplex.Example/Program.cs
+++ ExactSimplex.Example/Program.cs
@@ -1,11 +1,13 @@
 ﻿// Sample code to invoke the ExactSimplex class.
 // Problem description:
-// Get the exact solution of the following problem:
-// max z = 141𝑥1 + 393𝑥2 + 273𝑥3 + 804𝑥4 + 175𝑥5
-// 3𝑥1 + 5𝑥2 + 2𝑥3 + 5𝑥4 + 4𝑥5 ≤ 36
-// 7𝑥1 + 12𝑥2 + 11𝑥3 + 10𝑥4 ≤ 21
-// −3𝑥2 + 12𝑥3 + 7𝑥4 + 2𝑥5 ≤ 17
-// 0 ≤ 𝑥1, 𝑥2, 𝑥3, 𝑥4, 𝑥5 ≤ 20
+// Maximize:
+// P = 20x1 + 10x2 + 15x3
+// Subject to:
+// 3x1 + 2x2 + 5x3 <= 55
+// 2x1 + x2 + x3 <= 26
+// x1 + x2 + 3x3 <= 30
+// 5x1 + 2x2 + 4x3 <= 57
+// x1, x2, x3 >= 0
 
 using System;
 using System.Linq;
@@ -15,22 +17,16 @@ using Fractions;
 
 var constraints = new Constraint[]
 {
-	new Constraint(new Fraction[] { 3, 5, 2, 5, 4 }, new Fraction(36), "<="),
-	new Constraint(new Fraction[] { 7, 12, 11, 10, 0 }, new Fraction(21), "<="),
-	new Constraint(new Fraction[] { 0, -3, 12, 7, 2 }, new Fraction(17), "<="),
-	new Constraint(new Fraction[] { 1, 0, 0, 0, 0 }, new Fraction(20), "<="),
-	new Constraint(new Fraction[] { 1, 0, 0, 0, 0 }, new Fraction(0), ">="),
-	new Constraint(new Fraction[] { 0, 1, 0, 0, 0 }, new Fraction(20), "<="),
-	new Constraint(new Fraction[] { 0, 1, 0, 0, 0 }, new Fraction(0), ">="),
-	new Constraint(new Fraction[] { 0, 0, 1, 0, 0 }, new Fraction(20), "<="),
-	new Constraint(new Fraction[] { 0, 0, 1, 0, 0 }, new Fraction(0), ">="),
-	new Constraint(new Fraction[] { 0, 0, 0, 1, 0 }, new Fraction(20), "<="),
-	new Constraint(new Fraction[] { 0, 0, 0, 1, 0 }, new Fraction(0), ">="),
-	new Constraint(new Fraction[] { 0, 0, 0, 0, 1 }, new Fraction(20), "<="),
-	new Constraint(new Fraction[] { 0, 0, 0, 0, 1 }, new Fraction(0), ">=")
+	new Constraint(new Fraction[] { 3, 2, 5 }, new Fraction(55), "<="),
+	new Constraint(new Fraction[] { 2, 1, 1 }, new Fraction(26), "<="),
+	new Constraint(new Fraction[] { 1, 1, 3 }, new Fraction(30), "<="),
+	new Constraint(new Fraction[] { 5, 2, 4 }, new Fraction(57), "<="),
+	new Constraint(new Fraction[] { 1, 0, 0 }, new Fraction(0), ">="),
+	new Constraint(new Fraction[] { 0, 1, 0 }, new Fraction(0), ">="),
+	new Constraint(new Fraction[] { 0, 0, 1 }, new Fraction(0), ">=")
 };
 
-var function = new Function(new Fraction[] { 141, 393, 273, 804, 175 }, new Fraction(0), true);
+var function = new Function(new Fraction[] { 20, 10, 15 }, new Fraction(0), true);
 
 var simplex = new Simplex(function, constraints);
 
